37b73a705c8884f8747bfd71793baa07996d33b5
Jean-Philippe BOURGEON <jeanphilippe.bourgeon@gmail.com>
2021-04-17 08:57:32 +0200

    chore: switch to vscode dev container (#70)
    * chore: switch to vscode dev container
    * fix: ci test script

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+## SYSTEM DEPS
+cd /home/node
+apt-get update
+apt-get install git bash curl python make g++
+
+## HYPERFINE v1.2.0
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.2.0/hyperfine_1.2.0_amd64.deb
+dpkg -i hyperfine_1.2.0_amd64.deb
+
+## LAZYGIT v0.27.4
+wget https://github.com/jesseduffield/lazygit/releases/download/v0.27.4/lazygit_0.27.4_Linux_x86_64.tar.gz
+tar -xf lazygit_0.27.4_Linux_x86_64.tar.gz --directory /usr/bin
+
+## NPM
+sudo npm install -g npm
+
+# INSTALL AND BUILD POUCHDB FROM SOURCE
+cd /workspaces
+git clone --depth 1 https://github.com/pouchdb/pouchdb.git
+cd /workspaces/pouchdb
+sudo npm install
+sudo npm run build-node
+
+
+# INSTALL THE AND BUILD THE ROUTER
+cd /workspaces/pouchdb-nextjs-router
+sudo npm install
+sudo npm run build
